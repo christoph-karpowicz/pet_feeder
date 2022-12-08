a0895a7902306553abeed07590a0aa1e1c461854
christoph-karpowicz <christoph.karpowicz@gmail.com>
2022-12-08 18:17:04 +0100

    Turn the servo on every 20 seconds and turn it off with a button press.

--- main.c
+++ main.c
@@ -3,9 +3,14 @@
 #endif
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 
-#define KEY_PRESSED !(PIND & (1 << PD6))
+#define KEY_PRESSED !(PIND & (1 << PD3))
+#define PERIOD 20 // 28800
+
+volatile uint8_t timer_seconds;
+volatile uint8_t timer_ticks;
 
 void yellow_led_on() {
     PORTC |= (1 << PC0);
@@ -15,9 +20,29 @@ void yellow_led_off() {
     PORTC &= !(1 << PC0);
 }
 
+ISR(INT1_vect) {
+    yellow_led_off();
+    OCR1A = 0;
+}
+
+ISR(TIMER1_COMPA_vect) {
+    timer_ticks++;
+    if (timer_ticks >= 50) {
+        timer_seconds++;
+        timer_ticks = 0;
+    }
+    if (timer_seconds == PERIOD) {
+        if (OCR1A == 0) {
+            yellow_led_on();
+            OCR1A = 800;
+        }
+        timer_seconds = 0;
+    }
+}
+
 int main(void) {
     // Button
-    DDRD &= !(1 << PD6);
+    DDRD &= !(1 << PD3);
     // Yellow LED
     DDRC |= (1 << PC0);
 
@@ -34,16 +59,17 @@ int main(void) {
     OCR1A = 0;
     DDRD |= (1 << PD5);
 
-    _delay_ms(2000);
-    yellow_led_on();
+    // Interrupts
+    // Turn on the Timer/Counter1 Output Compare A match interrupt
+    TIMSK |= (1 << OCIE1A);
+    // Turn on interrupts on pin INT1
+    GICR |= (1 << INT1);
+    // Generate INT1 interrupt on falling egde
+    MCUCR |= (1 << ISC11);
+
+    sei();
+    _delay_ms(1000);
     while (1) {
-        if (KEY_PRESSED) {
-            OCR1A = 800;
-            yellow_led_off();
-        } else if (OCR1A != 0) {
-            OCR1A = 0;
-            yellow_led_on();
-        }
     }
     return 0;
 }
