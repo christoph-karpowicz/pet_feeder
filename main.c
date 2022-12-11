6827e33b1f13be858c976ea0ff0bfefca37663d4
christoph-karpowicz <christoph.karpowicz@gmail.com>
2022-12-11 19:05:25 +0100

    Turn on the servo on a single button press and turn it off on a double button press or INT0 interrupt (contact).

--- main.c
+++ main.c
@@ -6,11 +6,15 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-#define KEY_PRESSED !(PIND & (1 << PD3))
+#define BUTTON_PRESSED !(PIND & (1 << PD3))
+#define BUTTON_STANDBY_TIMER_TOP 50
 #define PERIOD 20 // 28800
 
 volatile uint8_t timer_seconds;
 volatile uint8_t timer_ticks;
+
+volatile uint8_t button_press_counter;
+volatile uint8_t button_wait_timer;
 
 void yellow_led_on() {
     PORTC |= (1 << PC0);
@@ -20,9 +24,16 @@ void yellow_led_off() {
     PORTC &= !(1 << PC0);
 }
 
-ISR(INT1_vect) {
-    yellow_led_off();
+ISR(INT0_vect) {
     OCR1A = 0;
+    yellow_led_off();
+}
+
+ISR(INT1_vect) {
+    if (button_wait_timer < BUTTON_STANDBY_TIMER_TOP - 10) {
+        button_press_counter++;
+        button_wait_timer = BUTTON_STANDBY_TIMER_TOP;
+    }
 }
 
 ISR(TIMER1_COMPA_vect) {
@@ -37,6 +48,20 @@ ISR(TIMER1_COMPA_vect) {
             OCR1A = 800;
         }
         timer_seconds = 0;
+    }
+
+    if (button_wait_timer > 0) {
+        button_wait_timer--;
+    }
+    if (button_press_counter > 0 && button_wait_timer == 0) {
+        if (button_press_counter == 2) {
+            yellow_led_off();
+            OCR1A = 0;
+        } else {
+            yellow_led_on();
+            OCR1A = 800;
+        }
+        button_press_counter = 0;
     }
 }
 
@@ -62,13 +87,15 @@ int main(void) {
     // Interrupts
     // Turn on the Timer/Counter1 Output Compare A match interrupt
     TIMSK |= (1 << OCIE1A);
-    // Turn on interrupts on pin INT1
-    GICR |= (1 << INT1);
+    // Turn on interrupts on pins INT0 and INT1
+    GICR |= (1 << INT0) | (1 << INT1);
+    // Generate INT0 interrupt on rising egde
+    MCUCR |= (1 << ISC01) | (1 << ISC00);
     // Generate INT1 interrupt on falling egde
     MCUCR |= (1 << ISC11);
 
-    sei();
     _delay_ms(1000);
+    sei();
     while (1) {
     }
     return 0;
