d3daedd30e6dfce55f1fd7b5253ebfdd46f2fb5a
Krzysztof Karpowicz <christoph.karpowicz@gmail.com>
2023-01-17 21:35:34 +0100

    Add I2C functions and enable interrupts on INT2.

--- main.c
+++ main.c
@@ -7,6 +7,9 @@
 #include <util/delay.h>
 #include <stdbool.h>
 
+#define DS1307_R 0xD1u 
+#define DS1307_W 0xD0u 
+
 #define BUTTON_STANDBY_TIMER_TOP 50
 #define BUTTON_PRESS_MANUAL_STOP 1
 #define BUTTON_PRESS_RESET_TIMER 2
@@ -84,15 +87,22 @@ void handle_led_blinking() {
     }
 }
 
+// External interrupt caused by a contact
 ISR(INT0_vect) {
     servo_off();
 }
 
+// External interrupt caused by a button
 ISR(INT1_vect) {
     if (button_wait_timer < BUTTON_STANDBY_TIMER_TOP - 10) {
         button_press_counter++;
         button_wait_timer = BUTTON_STANDBY_TIMER_TOP;
     }
+}
+
+// External interrupt caused by a DS1307 RTC clock
+ISR(INT2_vect) {
+    
 }
 
 ISR(TIMER1_COMPA_vect) {
@@ -122,6 +132,56 @@ ISR(TIMER1_COMPA_vect) {
     } else if (button_press_counter > 0 && button_wait_timer == 0) {
         handle_button_press();
     }
+}
+
+void I2C_init() {
+    // 1000000/(16+2*12*1) = 25Khz
+    TWBR = 12;
+}
+
+void I2C_start() {
+    TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
+    while (!(TWCR & (1 << TWINT)));
+}
+
+void I2C_stop() {
+    TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO);
+}
+
+void I2C_write(uint8_t data) {
+    TWDR = data;
+    TWCR = (1 << TWINT) | (1 << TWEN);
+    while (!(TWCR & (1 << TWINT)));
+}
+
+uint8_t I2C_read(uint8_t ack) {
+    if (ack) {
+        TWCR = (1 << TWINT) | (1 << TWEA) | (1 << TWEN);
+    } else {
+        TWCR = (1 << TWINT) | (1 << TWEN);
+    }
+    while (!(TWCR & (1 << TWINT)));
+    return TWDR;
+}
+
+void I2C_send(uint8_t addr, uint8_t data) {
+    I2C_start();
+    I2C_write(DS1307_W);
+    I2C_write(addr);
+    I2C_write(data);
+    I2C_stop();
+}
+
+void I2C_receive(uint8_t addr) {
+    I2C_start();
+    I2C_write(DS1307_W);
+    I2C_write(addr);
+    I2C_start();
+    I2C_write(DS1307_R);
+    uint8_t tmp;
+    tmp = I2C_read(0);
+    I2C_stop();
+    return tmp;
 }
 
 void disable_JTAG() {
@@ -158,8 +218,8 @@ void init() {
     // Interrupts
     // Turn on the Timer/Counter1 Output Compare A match interrupt
     TIMSK |= (1 << OCIE1A);
-    // Turn on interrupts on pins INT0 and INT1
-    GICR |= (1 << INT0) | (1 << INT1);
+    // Turn on interrupts on pins INT0, INT1 and INT2
+    GICR |= (1 << INT0) | (1 << INT1) | (1 << INT2);
     // Generate INT0 interrupt on rising egde
     MCUCR |= (1 << ISC01) | (1 << ISC00);
     // Generate INT1 interrupt on falling egde
@@ -169,6 +229,7 @@ void init() {
     sei();
 }
 
+// 1000000 / 64, 125
 int main(void) {
     init();
     while (1) {
